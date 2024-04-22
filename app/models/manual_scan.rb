049d660b3cec22ce885128e77f28c604210d0e75
Sujit Jagdale <jagdale.sujit@gmail.com>
2024-04-22 11:39:54 +0100

    created robot as well as manual scan models

---
+++ app/models/manual_scan.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Model to hold the manual scan data
+class ManualScan < Scan
+end
---
+++ app/models/robot_scan.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Model to hold the robot scan data
+class RobotScan < Scan
+end
